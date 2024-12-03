ff3673c90c6c1321f6fd6b334f2a25240bf1fde4
İrem Öğrünç <iremogrunc98@gmail.com>
2024-12-03 21:45:25 +0300

    Role model added

---
+++ app/Models/Role.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+use App\Models\User;
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+
+class Role extends Model
+{
+    /** @use HasFactory<\Database\Factories\UserFactory> */
+    use HasFactory;
+
+   public function users(){
+    return $this->belongsToMany(User::class);
+   }
+}
