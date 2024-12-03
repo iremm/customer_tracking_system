e08ce89122c15b9b908b4ac2c958f25615ce410c
İrem Öğrünç <iremogrunc98@gmail.com>
2024-12-03 22:24:24 +0300

    login method changed

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Hash;
 
 use Illuminate\Http\Request;
 use App\Models\User;
@@ -9,12 +10,25 @@ class UserController extends controller
 {
 
     public function login(Request $request){
-        if($request->role == 1){
-            dd("1");
+
+        $user = User::where('email',$request->email)->first();
+        if($user && Hash::check('password',$user->password)){
+            if($user->role == 1){
+                return response()->json([
+                    'status' => 'success',
+                    'role' => 1,
+                    'message' => "Success Login"
+                ]);
+            }
+            else{
+                return response()->json([
+                    'status' => 'success',
+                    'role' => 2,
+                    'message' => "Success Login"
+                ]);
+            }
         }
-        else{
-            dd("2");
-        }
+       
     }
 
 }
