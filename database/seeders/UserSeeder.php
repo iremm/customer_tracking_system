9bf20481c21b23db17d6f155e2662e0ce7bbf232
İrem Öğrünç <iremogrunc98@gmail.com>
2024-12-03 21:58:29 +0300

    seeders started

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -1,29 +1,36 @@
 <?php
 
 namespace Database\Seeders;
-
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeders;
 use Illuminate\Support\Facades\Hash;
 
-class UserSeeder extends Authenticatable
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
 {
    public function run(){
     //admin user
-    User::create([
+    $admin = User::create([
         'name' => 'Admin',
         'email' => 'admin@example.com',
         'password' => Hash::make('password'),
         'role' => 'admin'
     ]); 
     //customer user
-    User::create([
+    $staff = User::create([
         'name' => 'Staff',
         'email' => 'staff@example.com',
         'password' => Hash::make('password'),
         'role' => 'staff'
     ]); 
+
+    $adminRole = Role::where('name','admin')->first();
+    $staffRole = Role::where('name','staff')->first();
+
+    $admin->roles()->attach($adminRole);
+    $staff->roles()->attach($staffRole);
    }
 
 }
